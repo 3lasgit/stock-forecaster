e5d89cd2fe9c1be81d56339dab97234d2926f220
3la <blidiala32@gmail.com>
2023-12-16 22:38:52 +0100

    Changing the name of the generated file and adding tags where to create variables to make the file callable with arguments

--- data/data_loader.py
+++ data/data_loader.py
@@ -4,28 +4,35 @@ from pathlib import Path
 
 # URL de base
 base_url = "https://datasets-server.huggingface.co/rows?dataset=Ammok/apple_stock_price_from_1980-2021&config=default&split=train"
+# create a variable here to specify a wanted name for the file, function of the time series
 
 # Preparing loop's parameters
 response = requests.get(base_url)
 data = response.json()
-total_line = data['num_rows_total'] # we could request directly num_rows with the hugg_face params size but with this way we economize the first loading
-df = pd.json_normalize(data['rows'])  # initialiser la transformation des data en csv
+total_line = data[
+    "num_rows_total"]
+# we could request directly num_rows with the hugg_face params size but with this way we economize the first loading
+df = pd.json_normalize(data["rows"])  # initialiser la transformation des data en csv
 
 q = total_line // 100
 r = total_line % 100
-n_iter = q if r==0 else q + 1
+n_iter = q if r == 0 else q + 1
 
 # loading data sequentially
-for i in range(1,n_iter) :  # commence à 1 car on aura déjà fait au moins un request pour obtenir le total_line même s'il est nul
-    offset = i*100
+for i in range(
+    1, n_iter
+):  # commence à 1 car on aura déjà fait au moins un request pour obtenir le total_line même s'il est nul
+    offset = i * 100
     api_url = f"{base_url}&offset={offset}&length=100"
     response = requests.get(api_url)
     data = response.json()
-    df = pd.concat([df,pd.json_normalize(data['rows'])], axis = 0, ignore_index = True)
+    df = pd.concat([df, pd.json_normalize(data["rows"])], axis=0, ignore_index=True)
 
-df=df.drop(['row_idx','truncated_cells'], axis = 1)
+df = df.drop(["row_idx", "truncated_cells"], axis=1)
 
 # sortie
 output_dir = Path("%pwd").resolve().parent
-output_path = output_dir / "hugging_face_data.csv"
+output_path = (
+    output_dir / "apple_data.csv"
+)  # create a variable here to specify a wanted name for the file, function of the time series
 data_csv = df.to_csv(output_path, index=False)
